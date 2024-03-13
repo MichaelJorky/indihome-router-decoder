9c463591a453273b22fa98b50e18f55532a13013
MichaelJorky <90988612+MichaelJorky@users.noreply.github.com>
2024-03-13 16:27:45 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""setup.py module installer"""
+from setuptools import setup, find_packages
+
+with open('readme.md') as f:
+    readme = f.read()
+
+setup(
+    name='zcu',
+    description='ZTE Decoder Utility',
+    long_description=redame,
+    author='Dunia MR',
+    author_email='wgalxczk3@mozmail.com',
+    url='https://github.com/MichaelJorky/indihome-router-decoder',
+)
