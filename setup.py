d7b038a4d5ecd5e19254ffd18cdaaa2effdf6ce2
MichaelJorky <90988612+MichaelJorky@users.noreply.github.com>
2024-03-19 18:45:03 +0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 
 with open('README.md') as f:
     readme = f.read()
+with open('LICENSE') as f:
+    LICENSE = f.read()
+with open('requirements.txt') as f:
+    REQUIRED = f.read()
 
 setup(
     name='zcu',
@@ -11,4 +15,7 @@ setup(
     author='Dunia MR',
     author_email='wgalxczk3@mozmail.com',
     url='https://github.com/MichaelJorky/indihome-router-decoder',
+    license=LICENSE,
+    install_requires=REQUIRED,
+    packages=find_packages(exclude=('ftp', 'telnet'))
 )
